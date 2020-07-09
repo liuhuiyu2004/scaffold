d05a77255e4b4b99f77b7d1f197315cb0885e76a
liuhuiyu2004 <liuhuiyu2004@outlook.com>
2020-07-09 10:03:04 +0800

    加入本地时间工具

---
+++ src/main/java/com/liuhuiyu/scaffold/utils/DateTimeUtil.java
@@ -0,0 +1,9 @@
+package com.liuhuiyu.scaffold.utils;
+
+/**
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2020-07-09 9:51
+ */
+public class DateTimeUtil {
+}
---
+++ src/test/java/com/liuhuiyu/scaffold/utils/DateTimeUtilTest.java
@@ -0,0 +1,9 @@
+package com.liuhuiyu.scaffold.utils;
+
+/**
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2020-07-09 9:58
+ */
+public class DateTimeUtilTest {
+}
