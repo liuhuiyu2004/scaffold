86db182773be2b66c41347933922857d9427bf01
liuhuiyu2004 <liuhuiyu2004@outlook.com>
2020-07-10 08:36:26 +0800

    加入字节码工具类 加入访问地址工具类

--- src/test/java/com/liuhuiyu/scaffold/utils/DateTimeUtilTest.java
+++ src/test/java/com/liuhuiyu/scaffold/utils/DateTimeUtilTest.java
@@ -1,9 +1,30 @@
 package com.liuhuiyu.scaffold.utils;
 
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 /**
  * @author LiuHuiYu
  * @version v1.0.0.0
  * Created DateTime 2020-07-09 9:58
  */
 public class DateTimeUtilTest {
+    private final static Logger logger = LoggerFactory.getLogger(DateTimeUtilTest.class);
+    @Test
+    public void testStringToLocalDateTime() {
+        LocalDateTime localDateTime = DateTimeUtil.stringToLocalDateTime("2015-01-01");
+    }
+    @Test
+    public void testStringToLocalDate() {
+        LocalDate localDate = DateTimeUtil.stringToLocalDate("2015-01-01");
+    }
+    @Test
+    public void testStringToLocalTime() {
+        LocalTime localTime = DateTimeUtil.stringToLocalTime("2015-01-01");
+    }
 }
