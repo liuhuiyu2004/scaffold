806c99bddee63d8e9e66bcebb76bcea24156a4bd
liuhuiyu2004 <liuhuiyu2004@outlook.com>
2020-07-12 09:39:02 +0800

    更新本地日期时间工具

--- src/test/java/com/liuhuiyu/scaffold/utils/DateTimeUtilTest.java
+++ src/test/java/com/liuhuiyu/scaffold/utils/DateTimeUtilTest.java
@@ -1,5 +1,6 @@
 package com.liuhuiyu.scaffold.utils;
 
+import junit.framework.TestCase;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,18 +14,44 @@ import java.time.LocalTime;
  * @version v1.0.0.0
  * Created DateTime 2020-07-09 9:58
  */
-public class DateTimeUtilTest {
+public class DateTimeUtilTest extends TestCase {
     private final static Logger logger = LoggerFactory.getLogger(DateTimeUtilTest.class);
-    @Test
+
     public void testStringToLocalDateTime() {
         LocalDateTime localDateTime = DateTimeUtil.stringToLocalDateTime("2015-01-01");
+        logger.debug(localDateTime.toString());
     }
-    @Test
+
     public void testStringToLocalDate() {
         LocalDate localDate = DateTimeUtil.stringToLocalDate("2015-01-01");
+        logger.debug(localDate.toString());
     }
-    @Test
+
     public void testStringToLocalTime() {
         LocalTime localTime = DateTimeUtil.stringToLocalTime("2015-01-01");
+        logger.debug(localTime.toString());
     }
+
+    public void testLocalDateTimeToMilli() {
+
+    }
+
+    public void testLocalDateToMilli() {
+    }
+
+    public void testLocalTimeToMilli() {
+    }
+//    @Test
+//    public void testStringToLocalDateTime() {
+//
+//    }
+//    @Test
+//    public void testStringToLocalDate() {
+//
+//    }
+//    @Test
+//    public void testStringToLocalTime() {
+//
+//    }
+
 }
