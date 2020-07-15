da307197cb42dc55641aa25cec6654720be8ead9
liuhuiyu <liuhuiyu2004@outlook.com>
2020-07-15 14:53:23 +0800

    图形工具类加入 转换函数

--- src/main/java/com/liuhuiyu/scaffold/utils/ImageUtil.java
+++ src/main/java/com/liuhuiyu/scaffold/utils/ImageUtil.java
@@ -56,4 +56,89 @@ public class ImageUtil {
         ImageIO.write(imageBuffer, "jpg", byteOutputStream);
         return byteOutputStream.getBytes();
     }
+
+    /**
+     * 单字节 转换成 RGB 伪颜色
+     * @param val   单字节
+     * @return  RGB 伪颜色
+     */
+    public static int greyToRgb(int val) {
+        int r, g, b;
+
+        //red
+        if (val < 128) {
+            r = 0;
+        }
+        else if (val < 192) {
+            r = 255 / 64 * (val - 128);
+        }
+        else {
+            r = 255;
+        }
+        //green
+        if (val < 64) {
+            g = 255 / 64 * val;
+        }
+        else if (val < 192) {
+            g = 255;
+        }
+        else {
+            g = -255 / 63 * (val - 192) + 255;
+        }
+
+        //blue
+        if (val < 64) {
+            b = 255;
+        }
+        else if (val < 128) {
+            b = -255 / 63 * (val - 192) + 255;
+        }
+        else {
+            b = 0;
+        }
+//        Vec3b rgb;
+//        rgb[0] = b;
+//        rgb[1] = g;
+//        rgb[2] = r;
+        return hexToInt(convertRGBToHex(r, g, b));
+    }
+
+    /**
+     * 16进制字符串颜色转换成 int
+     *
+     * @param hex 16进制字符串
+     * @return 代表颜色的 int
+     */
+    public static int hexToInt(String hex) {
+        hex = hex.replace("#", "");
+        if (hex == "") return 0;
+        return Integer.parseInt(hex, 16);
+    }
+
+    /**
+     * RGB转换成十六进制
+     *
+     * @param r 红色
+     * @param g 绿色
+     * @param b 蓝色
+     * @return 16进制颜色字符串
+     */
+    public static @NotNull String convertRGBToHex(int r, int g, int b) {
+        String s = "0123456789ABCDEF";
+        String hex = "";
+        if (r >= 0 && r < 256 && g >= 0 && g < 256 && b >= 0 && b < 256) {
+            int x, y, z;
+            x = r % 16;
+            r = (r - x) / 16;
+            y = g % 16;
+            g = (g - y) / 16;
+            z = b % 16;
+            b = (b - z) / 16;
+            hex = s.substring(r, r + 1) + s.substring(x, x + 1) + s.substring(g, g + 1) + s.substring(y, y + 1) + s.substring(b, b + 1) + s.substring(z, z + 1);
+        }
+        else {
+            hex = "000000";
+        }
+        return hex;
+    }
 }
