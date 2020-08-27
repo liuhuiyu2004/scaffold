8fafd91ecc249b6c75090b573a81e9cfcd2da8a7
liuhuiyu <liuhuiyu2004@outlook.com>
2020-08-27 09:10:47 +0800

    加入rgb线性转换加入边界转换

--- src/main/java/com/liuhuiyu/scaffold/utils/ARGBLinearGradient.java
+++ src/main/java/com/liuhuiyu/scaffold/utils/ARGBLinearGradient.java
@@ -2,6 +2,7 @@ package com.liuhuiyu.scaffold.utils;
 
 /**
  * 使用线性渐变规则转换颜色
+ *
  * @author LiuHuiYu
  * @version v1.0.0.0
  * Created DateTime 2020-08-27 8:15
@@ -45,21 +46,41 @@ public class ARGBLinearGradient {
         this.r = r;
         this.g = g;
         this.b = b;
+        this.inMin = inMin;
+        this.scaleFactor = ((float) COLOR_COMPONENT_MAX) / (float) (inMax - inMin);
     }
 
-    public ARGBLinearGradient(LinearGradient a, LinearGradient r, LinearGradient g, LinearGradient b) {
+    public ARGBLinearGradient(LinearGradient a, LinearGradient r, LinearGradient g, LinearGradient b, int inMin, int inMax) {
         this.a = a;
         this.r = r;
         this.g = g;
         this.b = b;
+        this.inMin = inMin;
+        this.scaleFactor = ((float) COLOR_COMPONENT_MAX) / (float) (inMax - inMin);
     }
 
+    /**
+     * 获取转换后的ARGB数值
+     *
+     * @param value 输入值
+     * @return ARGB颜色
+     */
     public int getRgb(int value) {
-        int argb=0;
-        argb+=(this.a.getValue(value)<<24);
-        argb+=(this.r.getValue(value)<<16);
-        argb+=(this.g.getValue(value)<<8);
-        argb+=this.b.getValue(value);
+        int val;
+        if (value < this.inMin) {
+            val = this.inMin;
+        }
+        else if (value > this.inMax) {
+            val = this.inMax;
+        }
+        else {
+            val = (int) this.scaleFactor * (value - inMin);
+        }
+        int argb = 0;
+        argb += (this.a.getValue(val) << 24);
+        argb += (this.r.getValue(val) << 16);
+        argb += (this.g.getValue(val) << 8);
+        argb += this.b.getValue(val);
         return argb;
     }
 }
--- src/test/java/com/liuhuiyu/scaffold/utils/ARGBLinearGradientTest.java
+++ src/test/java/com/liuhuiyu/scaffold/utils/ARGBLinearGradientTest.java
@@ -16,14 +16,14 @@ public class ARGBLinearGradientTest extends TestCase {
         LinearGradient r = new LinearGradient(new int[]{0, 0, 255, 255});
         LinearGradient g = new LinearGradient(new int[]{0, 0, 255, 255});
         LinearGradient b = new LinearGradient(new int[]{0, 0, 255, 255});
-        ARGBLinearGradient argbLinearGradient = new ARGBLinearGradient(a, r, g, b);
+        ARGBLinearGradient argbLinearGradient = new ARGBLinearGradient(a, r, g, b, 0, 255);
         for (int i = 0; i < 255; i++) {
             log.debug("i=" + i + ";out = " + argbLinearGradient.getRgb(i));
         }
         int v = 255;
-        int j=0;
-        for(int i=0;i<4;i++){
-            j+=v<<(i*8);
+        int j = 0;
+        for (int i = 0; i < 4; i++) {
+            j += v << (i * 8);
         }
         log.debug("16进制输出 1<<8=" + Integer.toUnsignedString(j, 16));
         log.debug("2进制输出 1<<8=" + Integer.toUnsignedString(j, 2));
