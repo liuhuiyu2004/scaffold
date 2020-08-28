282c8276b7cd08ff14c3f26c47bc704b54a7c412
liuhuiyu <liuhuiyu2004@outlook.com>
2020-08-28 10:14:20 +0800

    改进ARGB渐变实现

--- src/main/java/com/liuhuiyu/scaffold/utils/ARGBLinearGradient.java
+++ src/main/java/com/liuhuiyu/scaffold/utils/ARGBLinearGradient.java
@@ -11,7 +11,7 @@ public class ARGBLinearGradient {
     /**
      * 分量最大值
      */
-    final int COLOR_COMPONENT_MAX = 255;
+    static final int COLOR_COMPONENT_MAX = 255;
     /**
      * 透明度线性渐变
      */
@@ -40,9 +40,22 @@ public class ARGBLinearGradient {
      * 输入输出比例系数
      */
     float scaleFactor;
+    /**
+     * 无渐变纯色
+     */
+    public final static LinearGradient SolidColor = new LinearGradient(new int[]{0, COLOR_COMPONENT_MAX, COLOR_COMPONENT_MAX, COLOR_COMPONENT_MAX});
 
+    /**
+     * 初始化线性渐变色谱
+     *
+     * @param r     红色分量
+     * @param g     绿色分量
+     * @param b     蓝色分量
+     * @param inMin 输入最小值
+     * @param inMax 输入最大值
+     */
     public ARGBLinearGradient(LinearGradient r, LinearGradient g, LinearGradient b, int inMin, int inMax) {
-        this.a = new LinearGradient(new int[]{0, COLOR_COMPONENT_MAX, COLOR_COMPONENT_MAX, COLOR_COMPONENT_MAX});
+        this.a = SolidColor;
         this.r = r;
         this.g = g;
         this.b = b;
